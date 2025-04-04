d75d596f8f88f6eaa6ea1e750d3204d52004ec2b
Lexicon121 <48660302+Lexicon121@users.noreply.github.com>
2025-04-04 14:24:33 -0500

    modified motor2 script inverted

--- launch/leftmotortest.py
+++ launch/leftmotortest.py
@@ -1,26 +1,26 @@
 from robot_hat import PWM, Pin
 import time
 
-# Manually test the left motor pins
 pwm = PWM('P13')       # Left motor PWM
 pwm.freq(50)
 dir_pin = Pin('D5')     # Left motor DIR
 
-def test_motor(speed):
-    if speed > 0:
+def test_left_motor(direction):
+    if direction == 'forward':
         dir_pin.value(1)
+        print("DIR set to HIGH (forward)")
     else:
         dir_pin.value(0)
-        speed = -speed
-    pwm.pulse_width_percent(speed)
+        print("DIR set to LOW (reverse)")
+    pwm.pulse_width_percent(50)
 
-print("Testing Left motor forward (should spin forward)")
-test_motor(50)
-time.sleep(2)
+print("Left motor: FORWARD")
+test_left_motor('forward')
+time.sleep(3)
 
-print("Testing Left motor reverse")
-test_motor(-50)
-time.sleep(2)
+print("Left motor: REVERSE")
+test_left_motor('reverse')
+time.sleep(3)
 
-print("Stopping motor")
-test_motor(0)
+print("STOP")
+pwm.pulse_width_percent(0)
